2ecf1b0b0b3359c62573c4769721c8ba6f0df347
Michele Pratusevich <mprat@mit.edu>
2014-09-26 14:36:06 -0400

    python script to find overlaping video ids in two text files

---
+++ python/overlap_vids.py
@@ -0,0 +1,9 @@
+f1 = open('../vids_to_download/ids_26-09-2014.txt')
+ids1 = f1.read().split('\n')
+f1.close()
+f = open('/Users/mprat/Desktop/ed-vids/names.txt')
+ids2 = f.read().split('\n')
+f.close()
+overlap = list(set(ids1) & set(ids2))
+overlap = [x for x in overlap if x != '']
+print "\nhttp://youtube.com/watch?v=".join(overlap)
