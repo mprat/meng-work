5e00198e1554eb2e90e7e7e5768e86b7327e6fc0
Michele Pratusevich <mprat@mit.edu>
2014-09-26 14:03:26 -0400

    startup.m file takes arguments

--- MATLAB/startup.m
+++ MATLAB/startup.m
@@ -1,3 +1,10 @@
-path_of_repo = '~/Desktop/mprat_home_afs/meng-work/';
-addpath([path_of_repo 'MATLAB/']);
-cd(path_of_repo);
+function startup(loc)
+	if strcmp(loc, 'afsmac')
+		path_of_repo = '~/Desktop/mprat_home_afs/meng-work/'
+	else
+		path_of_repo = '~/Documents/repos/meng-work/'
+	end
+
+	addpath([path_of_repo 'MATLAB/']);
+	cd(path_of_repo);
+end
