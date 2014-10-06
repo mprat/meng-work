b17783a6f557a9e4b50a67dd04c1b2dd8cfaeaef
Michele Pratusevich <mprat@mit.edu>
2014-10-06 17:43:35 -0400

    made command-line argument in download_video.py optional

--- python/download_video.py
+++ python/download_video.py
@@ -1,17 +1,25 @@
 import os
 import vidutils
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Break the given YouTube video into frames for later processing.')
-parser.add_argument('vid_URL', metavar="youtube_URL", type=str, help="The Youtube video link for the desired video. Should be of the form 'https://www.youtube.com/watch?v=aTuYZqhEvuk'.")
+parser.add_argument('--url', metavar="youtubeURL", type=str)
 # parser.add_argument('--ID', dest="id_given", const=True, default=False, help="Use this flag if you just want to use the youtube video ID.")
 
 args = parser.parse_args()
 vid_dir = '../../ed-vids'
-vid_name = 'ID-' + args.vid_URL[32:]
+
+# can either specify a command-line argument or type the name / ID of a video here
+if not args.url:
+	vid_name = 'ID-EMaTF9-ArJY'
+	vid_url = "http://youtube.com/watch?v=" + vid_name[3:]
+else:
+	vid_name = 'ID-' + args.url[32:]
+	vid_url = args.url
 
 # download video
-vidutils.download_video_url(args.vid_URL)
+vidutils.download_video_url(vid_url)
 # get length of video
 vid_duration = vidutils.get_video_length_secs(vid_name)
 # for loop to get all frames
