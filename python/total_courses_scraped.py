a6fa247b002c77f6571cc4e9807da4767a630fd0
Michele Pratusevich <mprat@mit.edu>
2014-11-03 16:58:56 -0500

    scraper updated slightly to be a little bit more tolerant

--- python/total_courses_scraped.py
+++ python/total_courses_scraped.py
@@ -1,7 +1,7 @@
 import json
 
 date_to_use = '11-03-2014'
-with open('all-courses-enhanced-' + date_to_use + '.json', 'r') as jsonfileprev
+with open('all-courses-enhanced-' + date_to_use + '.json', 'r') as jsonfileprev:
 	courses_prev = json.load(jsonfileprev)
 # jsonfileprev.close()
 
