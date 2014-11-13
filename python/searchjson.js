73c2a35ee9f068a858807dcaea9b7cb36205d77e
Michele Pratusevich <mprat@mit.edu>
2014-11-13 18:31:57 -0500

    ffmpeg can't be put inside a for loop. it can't be done.

--- python/searchjson.js
+++ python/searchjson.js
@@ -18,15 +18,31 @@ $( document ).ready(function() {
 
 
     $('#searchform').on('submit', function(ev){
+    	$('#results')
     	ev.preventDefault();
     	// console.log($('#query').val());
     	var regex = new RegExp($('#query').val());
-
+    	$('#results').text("")
+    	
     	var output = "";
 
     	$.getJSON('all-courses-enhanced-11-03-2014.json', function(data){
     		$.each(data, function(key, val){
-    			if ((val.code.search(regex) != -1) || (val.l.search(regex) != -1) || (val.url.search(regex) != -1)){
+    			var in_vid_ids = false;
+    			for (var i = val.vid_ids.length - 1; i >= 0; i--) {
+    				if (val.vid_ids[i].search(regex) != -1){
+    					in_vid_ids = true;
+    					break;
+    				}
+    			};
+                var in_vid_subtitles = false;
+                for (var i = val.subtitles.length - 1; i >= 0; i--) {
+                    if (val.subtitles[i].search(regex) != -1){
+                        in_vid_subtitles = true;
+                        break;
+                    }
+                };
+    			if ((val.code.search(regex) != -1) || (val.l.search(regex) != -1) || (val.url.search(regex) != -1) || in_vid_ids || in_vid_subtitles){
     				output += "<h1>"+val.l+"</h1>";
     				for (var i = val.vid_ids.length - 1; i >= 0; i--) {
     				 	output += "<p><a href='https://www.youtube.com/watch?v="+val.vid_ids[i]+"'>ID-"+val.vid_ids[i]+"</a></p>";
